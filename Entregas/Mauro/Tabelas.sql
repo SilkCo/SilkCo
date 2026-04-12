2ee2840fc16beaf60736a8013ea5a3349978dc38
Mauro-Neto <mauro.camposno@sptech.school>
2026-04-12 12:40:18 -0300

    Pequenos ajustes no Banco de dados

---
+++ Entregas/Mauro/Tabelas.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE silkCo;
+
+USE silkCo;
+
+CREATE TABLE empresa (
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+	telefone CHAR(13),
+    cnpj CHAR(14)
+);
+
+CREATE TABLE galpao (
+idGalpao INT PRIMARY KEY AUTO_INCREMENT,
+localizacao VARCHAR(40),
+fkEmpresa INT,
+CONSTRAINT chFkEmpresa
+	FOREIGN KEY(fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+CREATE TABLE usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(40) NOT NULL,
+    emailCadastro VARCHAR(40) NOT NULL UNIQUE,
+    emailSecundario VARCHAR(40) UNIQUE, 
+    senha VARCHAR(30) NOT NULL,
+    fkEmpresa INT,
+    CONSTRAINT chFkEmpresaUsuario 
+		FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+CREATE TABLE endereco (
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    logradoura VARCHAR(30),
+    numero INT,
+    cep CHAR(8),
+    fkEmpresa INT,
+    CONSTRAINT chFkEmpresaEndereco 
+		FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+CREATE TABLE sensor (
+    idSensor INT PRIMARY KEY AUTO_INCREMENT,
+    statusSensor VARCHAR(10) NOT NULL,
+    modeloSensor VARCHAR(30),
+	fkGalpao INT,
+    CONSTRAINT chStatus 
+        CHECK (statusSensor IN ('Ativo', 'Inativo', 'Manutencao')),
+	CONSTRAINT chFkGalpaoSensor 
+		FOREIGN KEY (fkGalpao) REFERENCES galpao (idGalpao)
+);
+
+CREATE TABLE leitura_sensor (
+    idColeta INT PRIMARY KEY AUTO_INCREMENT,
+    temperatura DECIMAL(4,2) NOT NULL,
+    umidade DECIMAL(4,2) NOT NULL,
+    dtColeta DATETIME NOT NULL,
+    fkSensor INT,
+    CONSTRAINT chFkSensor 
+		FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
+);
+
