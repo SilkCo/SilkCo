d453e10ddc9b3a4417b80a8bf1923353f909a6e5
theodoro-salvador <theodoro.salvador@sptech.school>
2026-05-21 12:57:46 -0300

    função login de usuário

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -19,8 +19,8 @@ function autenticar(req, res) {
                     
                     if (resultadoAutenticar.length == 1) {
                         
-                        res.json(resultadoAutenticar);
                         console.log(resultadoAutenticar);
+                        res.json(resultadoAutenticar);
                         
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
